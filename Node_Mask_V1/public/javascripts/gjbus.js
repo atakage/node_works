253175493a993d047fe9b163aa779e08201e68aa
atakage <atakoko2@gmail.com>
2020-03-12 16:45:32 +0900

    202003121645

---
+++ Node_Mask_V1/public/javascripts/gjbus.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.bus_tr').click(function(){
+
+
+        let id = $(this).data('id')
+        
+
+        $.ajax({
+            url : "/gjbus/bustime", data : {id:id}
+            ,success:function(result){
+                $('#bustime').html(result)
+            }, error:function(){
+                alert('서버에러')
+            }
+        })
+
+    })
+})
---
+++ Node_Mask_V1/routes/tmap_sec.js
@@ -0,0 +1,8 @@
+var tmap = {
+
+    api_key : "l7xx8dde1ae694ff4cb59d79b53e0ded0945"
+
+
+}
+
+module.exports = tmap
